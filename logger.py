b9e4dd9de5c6d4756ae4fa0ce3609383d9f91fa3
littlelightGit <vseprisebe@gmail.com>
2025-05-21 20:41:08 +0300

    feat: добавил /start

--- logger.py
+++ logger.py
@@ -1,5 +1,5 @@
 import logging
 
-logging.basicConfig(level=logging.INFO, filename="project.log", filemode="w",
+logging.basicConfig(level=logging.INFO, filename="project.log", filemode="a",
                     format="%(asctime)s %(levelname)s %(message)s")
 logger = logging.getLogger(__name__)
